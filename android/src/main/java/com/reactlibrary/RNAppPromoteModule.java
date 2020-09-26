d5be24ca2ec821eea15b90b77def05e8fff40f36
Steven J. Selcuk <stevenjselcuk@gmail.com>
2020-09-26 14:44:49 +0300

    Some Renaming + desc added

--- android/src/main/java/com/reactlibrary/RNAppPromoteModule.java
+++ android/src/main/java/com/reactlibrary/RNAppPromoteModule.java
@@ -6,17 +6,17 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
-public class RNAskForReviewModule extends ReactContextBaseJavaModule {
+public class RNAppPromoteModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
 
-  public RNAskForReviewModule(ReactApplicationContext reactContext) {
+  public RNAppPromoteModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
   }
 
   @Override
   public String getName() {
-    return "RNAskForReview";
+    return "RNAppPromote";
   }
 }
